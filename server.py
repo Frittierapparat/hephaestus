d752dc1db3b51e061e857c9a7c86b12c4499d4c1
Frittierapparat <72391367+Frittierapparat@users.noreply.github.com>
2021-10-14 22:09:20 +0200

    Add files via upload

---
+++ server.py
@@ -0,0 +1,64 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+import getData
+
+serverInfo = {
+    "hostname": "localhost",
+    "port": 8080
+}
+
+class myServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        #sending web interface contents (html, css, js, jquery)
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=utf-8")
+            self.end_headers()
+            self.wfile.write(open("website/index.html", "rb").read())
+        elif self.path == "/script.js":
+            self.send_response(200)
+            self.send_header("Content-type", "application/javascript; charset=utf-8")
+            self.end_headers()
+            self.wfile.write(open("website/script.js", "rb").read())
+        elif self.path == "/styles.css":
+            self.send_response(200)
+            self.send_header("Content-type", "text/css; charset=utf-8")
+            self.end_headers()
+            self.wfile.write(open("website/styles.css", "rb").read())
+        elif self.path == "/jquery":
+            self.send_response(200)
+            self.send_header("Content-type", "application/javascript; charset=utf-8")
+            self.end_headers()
+            self.wfile.write(open("website/jquery-3.6.0.min.js", "rb").read())
+        elif self.path == "/favicon":
+            self.send_response(200)
+            self.send_header("Content-type", "image/vnd.microsoft.icon")
+            self.end_headers()
+            self.wfile.write(open("website/favicon.ico", "rb").read())
+        elif self.path == "/icons/usb.svg":
+            self.send_response(200)
+            self.send_header("Content", "image/svg+xml")
+            self.end_headers()
+            self.wfile.write(open("icons/usb.svg", "rb").read())
+        #sending list of collected data to web interface
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json; charset=utf-8")
+            self.end_headers()
+            self.wfile.write(bytes(getData.getData(), "utf-8"))
+        else:
+            self.send_error(404)
+        
+            
+
+if __name__ == "__main__":
+    webServer = HTTPServer((serverInfo["hostname"], serverInfo["port"]), myServer)
+    print(time.asctime(), "Server started at http://%s:%s" % (serverInfo["hostname"], serverInfo["port"]))
+
+    try: 
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print(time.asctime(), "Server Stopped.")
---
+++ website/script.js
@@ -0,0 +1,46 @@
+var serverAddress = "http://localhost:8080";
+
+//function to create a dynamic list of gpus
+function showGpus(data){
+    for (let i = 0; i < Object.keys(data.gpu).length; i++){
+        $("#gpuData").append("<br><div id=\"gpu" + i + "\"></div>")
+        $("#gpu"+ i).append("<div id=\"gpu" + i + "Name\">" + data.gpu[i].vendor + " " + data.gpu[i].product + "</div>")
+    }
+};
+
+//function inserting data from the web server into the web interface
+function exportData(data){
+    $("#hostname").text(data.system.hostname);
+    $("#kernelVersion").text(data.system.os + " " + data.system.kernelVersion);
+    $("#architecture").text(data.system.architecture);
+    $("#locale").text(data.system.locale);
+    $("#cpuName").text(data.cpu.modelName);
+    $("#cpuCores").text(data.cpu.cores);
+    $("#cpuThreads").text(data.cpu.threads);
+    $("#cpuBoostFreq").text(data.cpu.boostFreq + "MHz");
+    $("#cpuCurrentFreq").text(data.cpu.currentFreq + "MHz");
+    $("#cpuFreqBar").attr("value", data.cpu.usage)
+    for (let i = 0; i < Object.keys(data.drives).length; i++){
+        if (data.drives[i].mountpoint === null){
+            $("#driveData").append("<br><div id=\"drive" + i + "\">" + data.drives[i].name + "</div>")
+        }
+        else{
+            $("#driveData").append("<br><div id=\"drive" + i + "\">" + data.drives[i].name + "(" + data.drives[i].mountpoint.substring(data.drives[i].mountpoint.search(/(?<=\/)[^/]+$/gm)) + ")" + "</div>")
+        }
+        if (data.drives[i].removable == true){
+            $("#drive" + i).append("<img src=\"icons/usb.svg\">")
+        }
+        if (data.drives[i].usage !== "None"){
+            $("#drive" + i).append("<progress id=\"drive" + i +"Prog\" min=\"0\" max=\"100\" value=\"" + data.drives[i].usage + "\"></progress>")
+        }
+    }
+    $("#cpuV").text(data.cpu.virtualization);
+    $("#cpuNuma").text(data.cpu.numaNodes);
+    $("#threadsPerCore").text(data.cpu.threadsPerCore);
+    $("#cpuVendor").text(data.cpu.vendorID);
+    showGpus(data)
+};
+
+fetch(serverAddress + "/data")
+    .then(response => response.json())
+    .then(data => exportData(data))
